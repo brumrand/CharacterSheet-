edbebc44b6369106889b76be1677ef117f73da55
Bruno <brunofragag99@gmail.com>
2023-06-07 23:11:11 +0200

    añadiendo estilos básicos

--- src/characterAttr.jsx
+++ src/characterAttr.jsx
@@ -2,15 +2,16 @@ export const CharacterAttr = ({ className, atributeName, AtributeValue}) =>{
     const smallAtribute = atributeName.substr(0,3).toUpperCase()
     return (
         <section className={className}>
+                <h2>
+                    {smallAtribute}
+                </h2>
             <div>
                 <h1 atribute={atributeName}>
                     {AtributeValue}
                 </h1>
             </div>
 
-                <h2>
-                    {smallAtribute}
-                </h2>
+
         </section>
     )
 }
