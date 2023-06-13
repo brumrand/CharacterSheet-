fd0c559cc47af18b166fc2328f52a27f87e982da
Bruno <brunofragag99@gmail.com>
2023-06-13 22:03:43 +0200

    estilado y creación de elemntos en el header

--- src/mainCharacterSheet.jsx
+++ src/mainCharacterSheet.jsx
@@ -1,19 +1,43 @@
 import {ProgressWithTitle} from "./progress"
+import {InputWithLabel} from "./progress"
+const variables=[
+    {
+        total : 120,
+        actual:100
+    },
+    {
+        total : 120,
+        actual:100
+    },
+]
 
 export const MainStrucutre = () =>{
     return (
         <main>
     <header>
     <section className='externalVariablesHedaerContainers'>
-            <div>
-                <ProgressWithTitle title="Vitalidad" max ="120" actual="110" textClass="headerVartext" progresClass="headerProgress"/>
+            <div className="variableDataHeader">
+                <ProgressWithTitle title="Vitalidad" max ={variables[0].total} actual={variables[0].actual} textClass="headerVartext" progresClass="headerProgress"/>
+                <InputWithLabel labelClasss={"headerLabelMax"} inputClass={"headerInputMax"} labelText={"MAX"} inputValue={variables[0].total} />
+                <InputWithLabel labelClasss={"headerLabelCur"} inputClass={"headerInputCur"} labelText={"CURRENT"} inputValue={variables[0].actual} />
             </div>
-            <div>
-                <ProgressWithTitle title="Estamina" max ="100" actual="85" textClass="headerVartext" progresClass="headerProgress" />
+            <div className="variableDataHeader">
+                <ProgressWithTitle title="Estamina" max ={variables[0].total} actual={variables[0].actual} textClass="headerVartext" progresClass="headerProgress" />
+                <InputWithLabel labelClasss={"headerLabelMax"} inputClass={"headerInputMax"} labelText={"MAX"} inputValue={variables[0].total} />
+                <InputWithLabel labelClasss={"headerLabelCur"} inputClass={"headerInputCur"} labelText={"CURRENT"} inputValue={variables[0].actual} />
             </div>
 
         </section>
         <section className='externalVariablesHedaerContainers'>
+            
+                <InputWithLabel labelClasss={"headerHakisLabel"} inputClass={"headerHakisInput"} labelText={"Armadura"} inputValue={5} />
+            
+            
+                <InputWithLabel labelClasss={"headerHakisLabel"} inputClass={"headerHakisInput"} labelText={"Observación"} inputValue={5} />
+            
+            
+                <InputWithLabel labelClasss={"headerHakisLabel"} inputClass={"headerHakisInput"} labelText={"Conquistador"} inputValue={5} />
+            
 
         </section>
 
--- src/progress.jsx
+++ src/progress.jsx
@@ -1,8 +1,19 @@
-export const ProgressWithTitle = ( title, max , actual, textClass, progresClass) =>{
+export const ProgressWithTitle = ({ title, max , actual, textClass, progresClass}) =>{
+    const numValue = parseInt(actual)
     return (
         < >
-
-            <progress  className={progresClass} max={max} value={actual}>  </progress>
+            <h3 className="textClass">{title}</h3>
+            <progress   className={progresClass} max={max} value={numValue}>  </progress>
         </>
+    )
+}
+
+export const InputWithLabel =({labelClasss, inputClass, labelText, inputValue}) =>{
+    return(
+        <>
+            <label className={labelClasss}>{labelText}</label>
+            <input className={inputClass} type="number"  value={inputValue}/>
+        </>
+
     )
 }
